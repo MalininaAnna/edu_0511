5e47e86cf5faa276a2df792ee941d5e69d2cd9ed
MalininaAnna <82583878+MalininaAnna@users.noreply.github.com>
2021-04-16 10:50:33 +0300

    Create Main.java

---
+++ задача 1.3/Main.java
@@ -0,0 +1,8 @@
+public  class  Main {
+    public  static  void  main ( String [] args ) {
+        String s ="Фотография";
+        int a =10;
+        int b =12;
+        System.out.println(s+" "+a+" на "+b);
+    }
+}
