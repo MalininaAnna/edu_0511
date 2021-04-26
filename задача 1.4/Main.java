d7626c28f016ce89cd0867b87ef4615c9cd7dcad
MalininaAnna <82583878+MalininaAnna@users.noreply.github.com>
2021-04-26 13:42:54 +0300

    Delete задача 1.4 directory

--- задача 1.4/Main.java
+++
@@ -1,8 +0,0 @@
-public  class  Main {
-    public  static  void  main ( String [] args ) {
-        int number =  25;
-        int a = 5;
-        a*=25;
-        System.out.println(number);
-        }
-}
