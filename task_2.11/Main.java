d99333efa545876e367a7967ef30f98627fd7887
MalininaAnna <82583878+MalininaAnna@users.noreply.github.com>
2021-04-26 17:20:21 +0300

    Create Main.java

---
+++ task_2.11/Main.java
@@ -0,0 +1,16 @@
+// НЕ ПОНИМАЮ КАК СДЕЛАТЬ
+/*2.11
+Задание: Дан массив с числами. Узнайте сколько элементов с начала массива надо сложить, чтобы в сумме получилось больше 10-ти.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        int nums [] = {1,2,3,4,5,6,7,8,9};
+        int sum = 0;
+        for (int i=0; i < nums.length; i++) {
+            sum+= nums[i];
+            if (sum > 10)
+        System.out.println(nums[i]);
+        }
+    }
+}
