74f5963708620d77c5044a5f207b99d9a63fb822
MalininaAnna <82583878+MalininaAnna@users.noreply.github.com>
2021-04-26 17:45:49 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int sum = 0;
         for (int i=0; i < nums.length; i++) {
             sum+= nums[i];
-            if (sum > 10)
+            if (sum <= 10)
         System.out.println(nums[i]);
         }
     }
