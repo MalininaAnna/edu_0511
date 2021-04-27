0704c507bbde6c1ffcb32ee94532a27b3fe30b08
MalininaAnna <82583878+MalininaAnna@users.noreply.github.com>
2021-04-27 15:29:24 +0300

    Update Main.java

--- task_3.2/Main.java
+++ task_3.2/Main.java
@@ -14,12 +14,18 @@
 */
 
 public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(min(-20, -10));
+        System.out.println(min(-40, -10, -30, 40));
+        System.out.println(min(-20, -40, -30, 40));
+        System.out.println(min(-20, -10, -40, 40));
+        System.out.println(min(-20, -10, -30, -40));
+    }
     public static int min(int a, int b, int c, int d) {
-            int x = min(a, b);
-            int y = min(c, d);
-            return min(x, y);
-        }
-  
+        int x = min(a, b);
+        int y = min(c, d);
+        return min(x, y);
+    }
     public static int min(int a, int b) {
         if (a < b) {
             return a;
@@ -27,13 +33,5 @@ public class Main {
         else {
             return b;
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        System.out.println(min(-20, -10));
-        System.out.println(min(-40, -10, -30, 40));
-        System.out.println(min(-20, -40, -30, 40));
-        System.out.println(min(-20, -10, -40, 40));
-        System.out.println(min(-20, -10, -30, -40));
     }
 }
