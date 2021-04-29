1724757e906c5943bf2574c32f97c3e62a4fab53
MalininaAnna <82583878+MalininaAnna@users.noreply.github.com>
2021-04-29 11:35:01 +0300

    Create Main.java

---
+++ task_3.3/Main.java
@@ -0,0 +1,12 @@
+/* 
+Задание: Сделайте функцию, которая параметрами принимает 2 числа. Если эти числа равны - пусть функция вернет true, а если не равны - false.
+*/
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(smooth(10,10));
+            }
+    static boolean smooth(int a, int b) {
+        return (a==b);
+    }
+}
